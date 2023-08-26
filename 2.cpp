dac42b4eafd6e416c719c61ddc3046ad7f25bf3a
Sahil <leviack629@gmail.com>
2023-08-26 20:34:30 +0530

    patterns

--- 2.cpp
+++ 2.cpp
@@ -125,12 +125,74 @@ void print11(int n){
 	}
 }
 
+void print12(int n){
+	int space = 2 *(n-1);
+	for(int i=1;i<=n;i++){
+		//numbers
+		for(int j=1; j<=i; j++){
+				cout << j;
+		}
+		//space
+		for(int j=1; j<=space; j++){
+			cout << " ";
+		}
+		//numbers
+		for(int j=i; j>=1; j--){
+				cout << j;
+		}
+		cout <<endl;
+		space -=2;
+	}
+}
+
+
+void print13(int n){
+	int num = 1;
+	for(int i =0; i<=n; i++){
+		for (int j=0; j<=i; j++){
+			cout << num << " ";
+			num = num +1 ;
+		}
+		cout << endl;
+	}
+}
+
+void print14(int n){
+	for(int i=0; i<=n; i++){
+		for(char ch = 'A'; ch <= 'A'+i; ch++){
+			cout << ch << " ";
+		}
+		cout <<endl;
+	}
+}
+
+
+void print15(int n){
+	for(int i=0;i<n;i++){
+		for(char ch = 'A'; ch <= 'A'+ (n-i-1); ch++){
+			cout << ch << " ";
+		}
+		cout << endl;
+	}
+}
+
+
+void print16(int n){
+	for(int i=0; i<n; i++){
+		char ch = 'A' +i;
+		for(int j=0;j<=i;j++){
+			cout << ch<< " ";
+		}
+		cout << endl;
+	}
+}
+
 int main() {
 	int t;
 	cin >> t;
 	for(int i=0; i<t; i++){
 		int n;
 		cin >> n;
-		print11(n);
+		print16(n);
 	}
 }
